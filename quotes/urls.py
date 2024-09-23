cfd4853e0bf7d80dfa7fa785a3989bbbe30bd56b
picoting <lindsayl.6857@gmail.com>
2024-09-23 17:19:40 -0400

    yay home page

--- quotes/urls.py
+++ quotes/urls.py
@@ -6,7 +6,8 @@ from . import views
 # all of the URLs that are part of this app
 urlpatterns = [
     path(r'', views.home, name="home"),
-    path(r'about', views.about, name="about"),
-    path('quotes/', include('quotes.urls')),
+    #path(r'about', views.about, name="about"),
+    #path(r'quote', views.quote, name="quote"),
+    #path(r'show_all', views.show_all, name="show_all"),
     
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+import time
+import random
 
 # Create your views here.
+
+quotes = [
+    "I sleep. I dream. I make up things that I would never say. I say them very quietly.",
+    "A man takes his sadness down to the river and throws it in the river, but then he’s still left with the river. A man takes his sadness and throws it away, but he’s still left with his hands.",
+    "You wanted to prove something, you wanted to start a war in my body, a revolution. I’m not brave anymore, darling. I’m not brave anymore.", 
+]
+
+images = [
+    "quotes/images/siken-1.jpg",
+    "quotes/images/siken-2.jpg",
+    "quotes/images/siken-3.jpg"
+]
+
+def home(request):
+    #select random quote
+    random_quote = random.choice(quotes)
+
+    #select random image
+    random_image = random.choice(images)
+
+
+    #time = time.ctime()
+
+    return render(request, 'quotes/home.html', {'quote': random_quote, 'image': random_image, 'current_time': time.ctime()})
