f2ae178f8d8c932518693aad0cc32cd30bcf9ef4
picoting <lindsayl.6857@gmail.com>
2024-10-07 17:23:51 -0400

    added all profiles

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path, include
 from django.conf import settings
 from . import views
+from .views import ShowAllProfilesView
 
 # all of the URLs that are part of this app
 urlpatterns = [
-    #path(r'', views.home, name="home"),
+    path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
     #path(r'about', views.about, name="about"),
     #path(r'quote', views.quote, name="quote"),
     #path(r'show_all', views.show_all, name="show_all"),   
